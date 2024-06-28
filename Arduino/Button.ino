8a7e6934ec04fb8980a004836c35b68361841c8a
André Almeida <andre.almeida@escolar.ifrn.edu.br>
2024-06-28 11:48:20 -0300

    Código Arduino

---
+++ Arduino/Button.ino
@@ -0,0 +1,65 @@
+/*
+  Button
+
+  Turns on and off a light emitting diode(LED) connected to digital pin 13,
+  when pressing a pushbutton attached to pin 2.
+
+  The circuit:
+  - LED attached from pin 13 to ground through 220 ohm resistor
+  - pushbutton attached to pin 2 from +5V
+  - 10K resistor attached to pin 2 from ground
+
+  - Note: on most Arduinos there is already an LED on the board
+    attached to pin 13.
+
+  created 2005
+  by DojoDave <http://www.0j0.org>
+  modified 30 Aug 2011
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Button
+*/
+
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 2;  // the number of the pushbutton pin
+const int ledPin = 13;    // the number of the LED pin
+const int ledRemoto=4;
+// variables will change:
+int buttonState = 0;  // Será usado para monitorar o estado do espaço físico
+int liga_desliga=0;
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  pinMode(ledRemoto,OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+
+  if (Serial.available()>0){
+    liga_desliga=Serial.read();
+    if(liga_desliga==49)
+      digitalWrite(ledRemoto, HIGH);
+    else
+      digitalWrite(ledRemoto, LOW);
+    Serial.println(liga_desliga);
+
+    delay(2000);
+  }
+
+}
